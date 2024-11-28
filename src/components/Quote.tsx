70742856fd62d58bb0a2501b3aa424bbf6373621
coror <coralic.erin@gmail.com>
2024-11-28 16:33:58 +0100

    trying to fix on mobile fixed image 2

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -20,24 +20,26 @@ export default function Quote({ quote }: { quote: string }) {
   return (
     <div className='relative w-full h-96 md:min-h-72 py-10 md:py-64'>
       <div
-        className="absolute inset-0 bg-[url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')] "
+        className='absolute top-0 left-0 w-full h-screen -z-10'
         style={{
+          backgroundImage: `url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')`,
           backgroundPosition: 'center',
           backgroundSize: 'cover',
-          backgroundAttachment: 'scroll',
+          backgroundAttachment: 'fixed',
         }}
       >
         <div className='absolute inset-0 bg-[#222428] opacity-65'></div>
-        <div
-          ref={ref}
-          className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center  md:text-[56px]  tracking-wide leading-[1] ${
-            animate
-              ? 'animate-fade-up animate-duration-[2000ms] animate-delay-[500ms]'
-              : ''
-          }`}
-        >
-          {quote}
-        </div>
+      </div>
+
+      <div
+        ref={ref}
+        className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center  md:text-[56px]  tracking-wide leading-[1] ${
+          animate
+            ? 'animate-fade-up animate-duration-[2000ms] animate-delay-[500ms]'
+            : ''
+        }`}
+      >
+        {quote}
       </div>
     </div>
   );
