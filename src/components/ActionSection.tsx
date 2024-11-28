ec2493df3b8cbbb3f4802bafd3c804672656dbfa
coror <coralic.erin@gmail.com>
2024-11-28 17:03:36 +0100

    trying to fix on mobile fixed image 5. for phone make scroll instead of 'fixed'

--- src/components/ActionSection.tsx
+++ src/components/ActionSection.tsx
@@ -26,51 +26,47 @@ export default function ActionSection({
   }, [inView]);
 
   const locale = useLocale();
-
+  const isMobile = window.innerWidth <= 768;
   return (
-    <div className="relative h-96 md:min-h-72 py-10 md:py-80 overflow-hidden">
-  {/* Fixed Background for ActionSection */}
-  <div
-    className="absolute inset-0 -z-10"
-    style={{
-      backgroundImage: `url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725620619/zumracoralic/IMG-d367d2aa0ebebb4e4cdbbe1872925ec5-V_bkht4p.jpg')`,
-      backgroundAttachment: 'fixed',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      clipPath: 'inset(0 0 0 0)', // Show the background only for this section
-    }}
-  ><div className="absolute inset-0 bg-[#222428] opacity-75"></div></div>
+    <div className='relative h-96 md:min-h-72 py-10 md:py-80 '>
+      <div
+        className="absolute inset-0 bg-[url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725620619/zumracoralic/IMG-d367d2aa0ebebb4e4cdbbe1872925ec5-V_bkht4p.jpg')]"
+        style={{
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundAttachment: isMobile ? 'scroll' : 'fixed'
+        }}
+      >
+        <div className='absolute inset-0 bg-[#222428] opacity-75'></div>
+        <div
+          className='relative flex flex-col items-center justify-center h-full text-center  md:px-40 '
+          ref={ref}
+        >
+          <div
+            className={`text-white text-2xl md:text-[56px] mt-20 tracking-wide leading-[1] mb-20 ${
+              animate
+                ? 'animate-fade-up animate-duration-[1000ms] animate-delay-[500ms]'
+                : ''
+            }`}
+          >
+            {quote}
+          </div>
 
-  {/* ActionSection Content */}
-  <div
-    ref={ref}
-    className="relative flex flex-col items-center justify-center h-full text-center md:px-40"
-  >
-    <div
-      className={`text-white text-2xl md:text-[56px] mt-20 tracking-wide leading-[1] mb-20 ${
-        animate
-          ? 'animate-fade-up animate-duration-[1000ms] animate-delay-[500ms]'
-          : ''
-      }`}
-    >
-      {quote}
+          <div
+            className={`${
+              animate
+                ? 'animate-fade-up animate-duration-[1000ms] animate-delay-[750ms]'
+                : ''
+            }`}
+          >
+            <Link href={`${locale}/contact`}>
+              <button className='bg-[#ffe6bc] px-5 py-4 md:px-6 md:py-5 text-sm md:mt-20 md:text-xl lg:text-2xl hover:scale-105 md:hover:scale-110 transition duration-150 ease-out hover:ease-in  flex items-center'>
+                {button}
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
-
-    <div
-      className={`${
-        animate
-          ? 'animate-fade-up animate-duration-[1000ms] animate-delay-[750ms]'
-          : ''
-      }`}
-    >
-      <Link href={`${locale}/contact`}>
-        <button className="bg-[#ffe6bc] px-5 py-4 md:px-6 md:py-5 text-sm md:mt-20 md:text-xl lg:text-2xl hover:scale-105 md:hover:scale-110 transition duration-150 ease-out hover:ease-in flex items-center">
-          {button}
-        </button>
-      </Link>
-    </div>
-  </div>
-</div>
-
   );
 }
--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
-import Image from 'next/image';
 
 export default function Quote({ quote }: { quote: string }) {
   const [animate, setAnimate] = useState(false);
@@ -10,6 +9,11 @@ export default function Quote({ quote }: { quote: string }) {
     triggerOnce: true,
     threshold: 0.1,
   });
+
+  const isMobile = window.innerWidth <= 768;
+
+
+  
 
   // When the component comes into view, set animate to true
   useEffect(() => {
@@ -19,33 +23,27 @@ export default function Quote({ quote }: { quote: string }) {
   }, [inView]);
 
   return (
-    <div className="relative overflow-hidden w-full h-96 md:min-h-72 py-10 md:py-64">
-  {/* Fixed Background for Quote */}
-  <div
-    className="absolute inset-0 -z-10"
-    style={{
-      backgroundImage: `url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')`,
-      backgroundAttachment: 'fixed',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      clipPath: 'inset(0 0 0 0)', // Show the background only for this section
-    }}
-  ><div className="absolute inset-0 bg-[#222428] opacity-75"></div></div>
-
-  {/* Quote Content */}
-  <div
-    ref={ref}
-    className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center md:text-[56px] tracking-wide leading-[1] ${
-      animate
-        ? 'animate-fade-up animate-duration-[2000ms] animate-delay-[500ms]'
-        : ''
-    }`}
-  >
-    {quote}
-  </div>
-</div>
-
-
-  
+    <div className='relative w-full h-96 md:min-h-72 py-10 md:py-64'>
+      <div
+        className="absolute inset-0 bg-[url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')] "
+        style={{
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+          backgroundAttachment: isMobile ? 'scroll' : 'fixed'
+        }}
+      >
+        <div className='absolute inset-0 bg-[#222428] opacity-65'></div>
+        <div
+          ref={ref}
+          className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center  md:text-[56px]  tracking-wide leading-[1] ${
+            animate
+              ? 'animate-fade-up animate-duration-[2000ms] animate-delay-[500ms]'
+              : ''
+          }`}
+        >
+          {quote}
+        </div>
+      </div>
+    </div>
   );
 }
