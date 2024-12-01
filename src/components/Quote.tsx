4f924f39cb0be5b18fa4a67ea8cd056d7ef9676b
coror <coralic.erin@gmail.com>
2024-12-01 13:27:05 +0100

    previous and next posts/blogs fixed

--- src/components/Quote.tsx
+++ src/components/Quote.tsx
@@ -1,19 +1,19 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useState, useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 export default function Quote({ quote }: { quote: string }) {
   const [animate, setAnimate] = useState(false);
-  const [isMobile, setIsMobile] = useState(false); // State to track if it's mobile
   const { ref, inView } = useInView({
     triggerOnce: true,
     threshold: 0.1,
   });
 
-  // Check if the viewport width is mobile size
-  useEffect(() => {
-    setIsMobile(window.innerWidth <= 768);
-  }, []);
+  const isMobile = window.innerWidth <= 768;
+
+
+  
 
   // When the component comes into view, set animate to true
   useEffect(() => {
@@ -25,17 +25,17 @@ export default function Quote({ quote }: { quote: string }) {
   return (
     <div className='relative w-full h-96 md:min-h-72 py-10 md:py-64'>
       <div
-        className="absolute inset-0 bg-[url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')]"
+        className="absolute inset-0 bg-[url('https://res.cloudinary.com/dbssbnuph/image/upload/v1725222544/zumracoralic/Untitled_Project_muob4p.jpg')] "
         style={{
           backgroundPosition: 'center',
           backgroundSize: 'cover',
-          backgroundAttachment: isMobile ? 'scroll' : 'fixed',
+          backgroundAttachment: isMobile ? 'scroll' : 'fixed'
         }}
       >
         <div className='absolute inset-0 bg-[#222428] opacity-65'></div>
         <div
           ref={ref}
-          className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center md:text-[56px] tracking-wide leading-[1] ${
+          className={`relative flex items-center justify-center h-full text-white p-10 text-3xl text-center  md:text-[56px]  tracking-wide leading-[1] ${
             animate
               ? 'animate-fade-up animate-duration-[2000ms] animate-delay-[500ms]'
               : ''
